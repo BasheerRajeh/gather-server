4b4f231699df88c352e44566e262b72d1f3b2ffb
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-07-10 16:34:43 +0300

    Installing needed libraries and start the server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server listening http://127.0.0.1:${PORT}`);
+});
